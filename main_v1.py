3b1d3486e19f0694031461f929c3ff4e70cfbdf1
erutan-lamfornok <branislav.nikol@gmail.com>
2017-02-25 16:12:39 +0100

    This main_v1.py is witten to be compatible with slither_v1.py
    Main differences with main.py:
    
    1.  dt variable unnecessary, because update() no longer needs it (see
    update() function in slither_v1.py)
    
    2. version 'slither_v1' added as a choice of versions (see tools in
    utils, where the key 'v' is defined for slither_v1.py)

---
+++ main_v1.py
@@ -0,0 +1,69 @@
+import pygame, sys
+sys.path.append('E:/pPYTHON stuff/Game making/Slither')
+from utils import tools
+
+pygame.init()  # initiates all inside pygame; returns a tuple
+
+# loading graphic objects
+apple = pygame.image.load('E:/pPYTHON stuff/Game making/Slither/apple.png')
+img = pygame.image.load('E:/pPYTHON stuff/Game making/Slither/snake_head.png')
+
+# defining game dimensions
+block_size = 30 # apple.get_width() #20
+steps = [15, 10]
+display_size = [steps[0] * block_size, steps[1] * block_size]
+
+# defining colors
+colors = dict(white = (255, 255, 255),
+             black = (0, 0, 0),
+             red = (255, 0, 0),
+             green = (0, 155, 0))
+
+# Defining custom fonts
+myfonts = dict(smallfont = pygame.font.SysFont("comicsansms", 15),
+               medfont = pygame.font.SysFont("comicsansms", 30),
+               bigfont = pygame.font.SysFont("comicsansms", 50))
+
+# Global gameplay variables
+direction = "right"
+level = 0
+FPS = 5
+
+# initializing window for display
+screen = pygame.display.set_mode(display_size)
+clock = pygame.time.Clock()
+version = tools.game_intro(display_size, colors, myfonts, screen)
+
+if version[0] == 'simple':
+    from versions import simple
+    game = simple.gameplay(steps, block_size)
+elif version[0] == 'slither_v1':
+    from versions import slither_v1
+    game = slither_v1.gameplay(steps, block_size)
+elif version[0] == 'slither':
+    pygame.quit()
+    sys.exit('Slither is not hungry yet! Play the crude version!')
+#     from versions import slither
+#     game = slither.gameplay(block_size, display_size)
+switch = True
+
+while switch:
+    clock.tick(FPS)
+
+    events = pygame.event.get()
+    game.update(events)
+
+    for event in events:
+        if event.type == pygame.QUIT:
+            switch = False
+            pygame.quit()
+            sys.exit('you exited the game')
+        elif event.type == pygame.mouse.get_focused:
+            continue
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                tools.pause(colors, myfonts, display_size, screen)
+
+    screen.fill(colors['white'])
+    game.draw_to_screen(screen)
+    pygame.display.flip()
